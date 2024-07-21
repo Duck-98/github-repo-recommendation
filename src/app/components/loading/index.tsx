d86fd4b7479a53ef841b77626a5a421df46e05d4
duck <godqhr2256@gmail.com>
2024-07-21 22:38:54 +0900

    feat: loading component

---
+++ src/app/components/loading/index.tsx
@@ -0,0 +1,5 @@
+export const Loading = () => (
+  <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="loading loading-spinner loading-lg"></div>
+  </div>
+);
